f8b6c6986d7e9b985b2d58d9918ebd09fd1ee5ff
Kolmagin69 <nikone.12.95@yandex.ru>
2020-06-06 01:47:34 +0300

    add Spring Boot resolve #1

---
+++ src/main/java/learning/java/game/GameApplication.java
@@ -0,0 +1,15 @@
+package learning.java.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@SpringBootApplication
+public class GameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameApplication.class, args);
+	}
+
+}
