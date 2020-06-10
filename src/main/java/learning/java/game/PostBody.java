7b33c49de5cc1790e3d924245961b760d3a4de26
Kolmagin69 <nikone.12.95@yandex.ru>
2020-06-11 00:34:04 +0300

    create class PostBody which parse JSON from request to java class

---
+++ src/main/java/learning/java/game/PostBody.java
@@ -0,0 +1,13 @@
+package learning.java.game;
+
+public class PostBody {
+    private String side;
+
+    public String getSide() {
+        return side;
+    }
+
+    public void setSide(String side) {
+        this.side = side;
+    }
+}
