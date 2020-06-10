8fe8b3a0142a13d91f760cc939f960159ba4c64e
Kolmagin69 <nikone.12.95@yandex.ru>
2020-06-11 00:34:18 +0300

    create class TurnBody which parse JSON from request to java class

---
+++ src/main/java/learning/java/game/TurnBody.java
@@ -0,0 +1,26 @@
+package learning.java.game;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class TurnBody {
+    private int[] position;
+
+    public int[] getPosition() {
+        return position;
+    }
+
+    public void setPosition(int[] position) {
+        this.position = position;
+    }
+
+    public int getX() {
+        return position[0];
+    }
+
+
+
+    public int getY() {
+        return position[1];
+    }
+
+}
