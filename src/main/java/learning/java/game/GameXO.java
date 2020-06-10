6f1646957303b33b80959eb9dac7fa4f2c49880b
Kolmagin69 <nikone.12.95@yandex.ru>
2020-06-11 00:38:41 +0300

    fix method setTurn, setField, add method gameDefault which return new GameXO whit default settings

--- src/main/java/learning/java/game/GameXO.java
+++ src/main/java/learning/java/game/GameXO.java
@@ -1,5 +1,9 @@
 package learning.java.game;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.Random;
+
 public class GameXO {
     private final String id;
     private final String type;
@@ -8,6 +12,8 @@ public class GameXO {
     private final Player playerO;
     private final String[][] field = new String[3][3];
     private String turn;
+    @JsonIgnore
+    private String nextTurn;
     private Player winner;
 
     public GameXO(String id, String type, String name, Player playerX, Player playerO) {
@@ -48,11 +54,25 @@ public class GameXO {
 
     public void setField(int x, int y) {
         field[x][y] = turn;
+        nextTurn(field);
+        String isWin = WinnerController.checkLineWinner(field);
+        if ( isWin != null) {
+            if (isWin.equals("X"))
+                winner = playerX;
+            if (isWin.equals("O"))
+                winner = playerO;
+        }
     }
 
     public void setTurn(String turn) {
         this.turn = turn;
-    }
+        if (turn.equals("X") && turn.equals("O"))
+            throw new RuntimeException();
+        if (turn.equals("X"))
+            nextTurn = "O";
+        if (turn.equals("O"))
+            nextTurn = "X";
+        }
 
     public Player getWinner() {
         return winner;
@@ -60,5 +80,25 @@ public class GameXO {
 
     public void setWinner(Player winner) {
         this.winner = winner;
+    }
+    public static GameXO gameDefault() {
+        return new GameXO("e379002d-6fba-4899-96b2-38371f458418",
+                "singleplayer",
+                "10235",
+                new Player("player", "2a5ba390-c7b3-40bf-903c-8fba36913092"),
+                new Player("AI", null));
+    }
+    public void nextTurn (String[][] field) {
+        Random randomInt = new Random();
+        int check = -1;
+        while (check == -1) {
+            int x = randomInt.nextInt(3);
+            int y = randomInt.nextInt(3);
+            if (field[x][y] == null) {
+                field[x][y] = nextTurn;
+                check = 1;
+            }
+        }
+
     }
 }
