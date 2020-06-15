d4fef37df35e8fee7e99e1e18d1edfa81035d928
Kolmagin69 <Trek6969Trek>
2020-06-16 02:13:04 +0300

    add interface GameController

---
+++ src/main/java/learning/java/game/gameModel/controller/GameController.java
@@ -0,0 +1,9 @@
+package learning.java.game.gameModel.controller;
+
+import learning.java.game.gameModel.model.Game;
+import learning.java.game.gameModel.model.Point;
+
+public interface GameController {
+
+    public void letsPlay (Game game, Point point);
+}
