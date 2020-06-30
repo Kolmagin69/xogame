18d2ad1a8e68d510eda1a327b0e168b13bee85bc
Kolmagin69 <nikone.12.95@yandex.ru>
2020-06-30 10:46:51 +0300

    move all logic in GameServiceSingle related to GameController and Dao

--- src/main/java/learning/java/game/rest_controller/request_body/GameRestController.java
+++ src/main/java/learning/java/game/rest_controller/request_body/GameRestController.java
@@ -2,6 +2,7 @@ package learning.java.game.rest_controller.request_body;
 
 import learning.java.game.dao.GameDao;
 import learning.java.game.game_model.controller.GameController;
+import learning.java.game.game_model.controller.GameService;
 import learning.java.game.game_model.model.CreateGame;
 import learning.java.game.game_model.model.Figure;
 import learning.java.game.game_model.model.Game;
@@ -14,46 +15,26 @@ import java.util.UUID;
 @RestController
 @RequestMapping("game")
 public class GameRestController {
+
     @Autowired
-    private GameController gameControl;
-    @Autowired
-    private CreateGame gameXO;
-    @Autowired
-    private GameDao dao;
+    GameService gameService;
 
     @GetMapping("{id}")
     @ResponseBody
     public Game gameFromId(@PathVariable String id) {
-        return dao.getDataBase().get(UUID.fromString(id));
+        return gameService.getGameFromId(id);
     }
 
     @PostMapping
     public @ResponseBody Game postGame(@RequestBody PostBody postBody) {
-        Figure postFigure = postBody.getSide();
-
-        if (postFigure == null)
-            throw new IllegalArgumentException();
-
-        Game game = gameXO.newGame(postFigure);
-        game.setTurn(gameControl.currentFigure(game.getField()));
-        dao.setDataBase(game.getId(), game);
-        return game;
-
+        return gameService.postNewGame(postBody);
     }
 
     @PostMapping("{id}/turn")
     public  @ResponseBody Game turn(@RequestBody TurnBody turnBody,
                                      @PathVariable String id) {
-        if (turnBody == null || id == null)
-            throw new IllegalArgumentException();
 
-        Game gameXO = dao.getDataBase().get(UUID.fromString(id));
-        if (gameXO == null)
-            throw new IllegalArgumentException();
-
-        Point point = new Point(turnBody.getX(), turnBody.getY());
-        gameControl.letsPlay(gameXO, point);
-        return gameXO;
+        return gameService.turnGameFromId(turnBody, id);
     }
 
 
