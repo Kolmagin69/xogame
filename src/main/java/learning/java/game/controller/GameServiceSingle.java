fc72c514157feb8928f063c2997aa31ab9a3cd65
Kolmagin69 <nikone.12.95@yandex.ru>
2020-07-04 15:34:08 +0300

    add exception message, change some IllegalArgumentException to NotFoundException, add NotFoundExceptions to method getGameFromId

--- src/main/java/learning/java/game/controller/GameServiceSingle.java
+++ src/main/java/learning/java/game/controller/GameServiceSingle.java
@@ -1,6 +1,7 @@
 package learning.java.game.controller;
 
 import learning.java.game.dao.Dao;
+import learning.java.game.exception.NotFoundExceptions;
 import learning.java.game.model.CreateGame;
 import learning.java.game.model.Figure;
 import learning.java.game.model.Game;
@@ -22,11 +23,17 @@ public class GameServiceSingle implements GameService {
     private CreateGame gameXO;
 
     @Autowired
-    private Dao dao;
+    private Dao<Game, UUID> dao;
 
     @Override
     public Game getGameFromId(String id) {
-        return (Game) dao.read(UUID.fromString(id));
+        UUID key = UUID.fromString(id);
+        Game game = dao.read(key);
+
+        if(game == null)
+            throw new NotFoundExceptions("Not found game with this id: " + key);
+
+        return dao.read(key);
     }
 
     @Override
@@ -34,7 +41,8 @@ public class GameServiceSingle implements GameService {
         Figure postFigure = postBody.getSide();
 
         if (postFigure == null)
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("Sent request with incorrect body. " +
+                    "Try - {\"side\":\"X\"}");
 
         Game game = gameXO.newGame(postFigure);
         game.setTurn(gameControl.currentFigure(game.getField()));
@@ -44,12 +52,15 @@ public class GameServiceSingle implements GameService {
 
     @Override
     public Game turnGameFromId(TurnBody turnBody, String id) {
-        if (turnBody == null || id == null)
-            throw new IllegalArgumentException();
-
-        Game gameXO = (Game) dao.read(UUID.fromString(id));
+        if (turnBody == null)
+            throw new IllegalArgumentException("Sent request with incorrect body." +
+                    " Try - {\"position\":\"[2,2]\"}");
+        if (id == null)
+            throw new IllegalArgumentException("Sent request to incorrect address." +
+                    " Try - game/{UUID}/turn");
+        Game gameXO = dao.read(UUID.fromString(id));
         if (gameXO == null)
-            throw new IllegalArgumentException();
+            throw new NotFoundExceptions("Game with id: \"" + id + "\" Not found");
 
         Point point = new Point(turnBody.getX(), turnBody.getY());
         gameControl.letsPlay(gameXO, point);
