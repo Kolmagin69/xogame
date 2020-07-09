110c1e95bb1e7f24668ddaa0b314892ea1afb95d
Kolmagin69 <nikone.12.95@yandex.ru>
2020-07-09 20:19:18 +0300

    remove some @Autowired, refactoring all methods and add one @Test

--- src/test/java/learning/java/game/GameApplicationTests.java
+++ src/test/java/learning/java/game/GameApplicationTests.java
@@ -1,14 +1,7 @@
 package learning.java.game;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.exc.MismatchedInputException;
-import learning.java.game.model.Field;
-import learning.java.game.model.Figure;
-import learning.java.game.model.Game;
-import learning.java.game.model.Point;
-import learning.java.game.rest.GameRestController;
-import learning.java.game.rest.PostBody;
-import learning.java.game.rest.TurnBody;
+import learning.java.game.dao.Dao;
+import learning.java.game.model.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,11 +11,17 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultMatcher;
 
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -34,154 +33,176 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class GameApplicationTests {
 
     @Autowired
-    private GameRestController gameController;
-
-    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
-    private ObjectMapper objectMapper;
+    private Dao<Game, UUID> dao;
 
     @Test
     void testPostGame() throws Exception {
-        Figure inputFigure = Figure.O;
-        PostBody postBody = new PostBody(){{
-            setSide(inputFigure);
-        }};
+        String request = readFromJson("/post/create/create_game_request.json");
+        String response = readFromJson("/post/create/create_game_response.json");
 
-        MvcResult result = this.mockMvc.perform(post("/game")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(postBody)))
-                .andExpect(status().isOk())
-                .andReturn();
-
-        Game game = gameFromMvcResult(result);
-        Figure actualFigure = game.getPlayers().get(0).getFigure();
-        Figure unexpectedFigure = Figure.X;
-
-        assertEquals(actualFigure, inputFigure);
-        assertFalse(actualFigure == unexpectedFigure);
+        mockMvcPostRequest("/game", request, response, status().isOk());
     }
 
     @Test
     void testFailIfPostRequestWithIncorrectBody() throws Exception {
-        try {
-            String requestBody = "{\"side\":\"P\"}";
-            MvcResult result = postIncorrectBody(requestBody);
-            Game game = gameFromMvcResult(result);
-            fail();
-        } catch (MismatchedInputException e) {
-        }
+        String url = "/game";
+        String request1 = readFromJson("/post/create/create_incorrect_request1.json");
+        String request2 = readFromJson("/post/create/create_incorrect_request2.json");
+        String request3 = readFromJson("/post/create/create_incorrect_request3.json");
+        String response = readFromJson("/bad.response/bad_request.json");
+        ResultMatcher badRequest = status().isBadRequest();
 
-        try {
-            String requestBody = "{\"something\":\"O\"}";
-            postIncorrectBody(requestBody);
-            fail();
-        } catch (Exception e) {
-
-        }
-
-        postIncorrectBody("");
+        mockMvcPostRequest(url, request1, response, badRequest);
+        mockMvcPostRequest(url, request2, response, badRequest);
+        mockMvcPostRequest(url, request3, response, badRequest);
     }
 
     @Test
     void testGameFromId() throws Exception {
-        Game expectedGame = postAndReturnGame();
-        String urlTemplate = "/game/" + expectedGame.getId();
-        MvcResult result = this.mockMvc.perform(get(urlTemplate))
-                .andExpect(status().isOk())
-                .andReturn();
+        String id = createGameAndReturnId(dao);
+        String urlTemplate = "/game/" + id;
+        String response = readFromJson("/get/get_game_response.json").replace("${id}", id);
 
-        Game actualGame = gameFromMvcResult(result);
-        assertEquals(actualGame.getId(), expectedGame.getId());
-
+        mockMvcGetRequest(urlTemplate, response, status().isOk());
     }
 
     @Test
     void testFailTryGetGameWithIncorrectId() throws Exception {
-        Game game = postAndReturnGame();
+        String id = createGameAndReturnId(dao);
+        String urlTemplateCorrect = "/game/" + id;
+        String urlTemplateIncorrect1 = "/game/" + UUID.randomUUID();
+        String urlTemplateIncorrect2 = "/game/" + "Incorrect path";
 
-        String urlTemplateIncorrect = "/game/" + UUID.randomUUID();
-        String urlTemplateCorrect = "/game/" + game.getId();
-        MvcResult result1 = this.mockMvc.perform(get(urlTemplateCorrect))
-                .andExpect(status().isOk())
-                .andReturn();
-        Game game1 = gameFromMvcResult(result1);
-        assertEquals(game.getId(), game1.getId());
+        String responseCorrect = readFromJson("/get/get_game_response.json")
+                .replace("${id}", id);
+        String responseIncorrect1 = readFromJson("/bad.response/not_found.json");
+        String responseIncorrect2 = readFromJson("/bad.response/bad_request.json");
 
-        this.mockMvc.perform(get(urlTemplateIncorrect))
-                .andExpect(content().bytes(new byte[0]));
+        mockMvcGetRequest(urlTemplateCorrect, responseCorrect,status().isOk());
+        mockMvcGetRequest(urlTemplateIncorrect1, responseIncorrect1, status().isNotFound());
+        mockMvcGetRequest(urlTemplateIncorrect2, responseIncorrect2, status().isBadRequest());
     }
 
     @Test
     void testTurn() throws Exception {
-        Game game  = postAndReturnGame();
-        TurnBody turnBody = new TurnBody(2) {{
-            setX(0);
-            setY(0);
-        }};
+        //create game and check figure into games field on point(2,2)
+        Game game = createGameXO(dao);
+        Point point = new Point(2, 2);
+        Figure figureOld = game.getField().getFigure(point);
+        assertNull(figureOld);
 
-        String urlTemplate = "/game/" + game.getId() + "/turn";
-        MvcResult result = this.mockMvc.perform(post(urlTemplate)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(turnBody)))
-                .andExpect(status().isOk())
-                .andReturn();
+        //make post request and check response with out games field,
+        //because when we set figure into field GameService set one more random figure
+        String id = game.getId().toString();
+        String url = "/game/" + id + "/turn";
+        String request = readFromJson("/post/turn/turn_game_request.json");
+        String response = readFromJson("/post/turn/turn_game_response.json")
+                .replace("${id}", id);
 
-        Game actualGame = gameFromMvcResult(result);
-        Field expectedField = new Field(3){{
-            setFigures(new Point(0, 0), Figure.X);
-        }};
-        Figure[][] actualFigures = actualGame.getField().getFigures();
-        Figure[][] expectedFigures = expectedField.getFigures();
+        mockMvcPostRequest(url, request, response, status().isOk());
 
-        assertEquals(actualFigures[0][0], expectedFigures[0][0]);
-        assertFalse(Arrays.equals(actualFigures, expectedFigures));
+        //repeatedly check figure on point(2,2)
+        Figure figureActual = game.getField().getFigure(point);
+        assertEquals(Figure.O, figureActual);
     }
 
     @Test
     void testFailWhenTryTurnWIthIncorrectGameId() throws Exception {
-        postAndReturnGame();
-        TurnBody turnBody = new TurnBody(2) {{
-            setX(1);
-            setY(2);
+        String id = createGameAndReturnId(dao);
+        String url = "/game/" + id + "/turn";
+        String urlBad1 = "/game/" + UUID.randomUUID() + "/turn";
+        String urlBad2 = "/game/" + "Incorrect path" + "/turn";
+
+        String request = readFromJson("/post/turn/turn_game_request.json");
+        String response = readFromJson("/post/turn/turn_game_response.json")
+                .replace("${id}", id);
+        String responseBad1 = readFromJson("/bad.response/not_found.json");
+        String responseBad2= readFromJson("/bad.response/bad_request.json");
+
+        mockMvcPostRequest(url, request, response, status().isOk());
+        mockMvcPostRequest(urlBad1, request, responseBad1, status().isNotFound());
+        mockMvcPostRequest(urlBad2, request, responseBad2, status().isBadRequest());
+    }
+
+    @Test
+    void testFailWhenTryTurnWIthIncorrectBodyRequest() throws Exception {
+        String id = createGameAndReturnId(dao);
+        String url = "/game/" + id + "/turn";
+
+        String requestBad1 = readFromJson("/post/turn/turn_incorrect_request1.json");
+        String requestBad2 = readFromJson("/post/turn/turn_incorrect_request2.json");
+        String requestBad3 = readFromJson("/post/turn/turn_incorrect_request3.json");
+        String response = readFromJson("/bad.response/bad_request.json");
+        ResultMatcher badRequest = status().isBadRequest();
+
+        mockMvcPostRequest(url, requestBad1, response, badRequest);
+        mockMvcPostRequest(url, requestBad2, response, badRequest);
+        mockMvcPostRequest(url, requestBad3, response, badRequest);
+
+    }
+
+    //further are private methods
+
+    private Game createGameXO(Dao dao) {
+        Game game = new Game(
+                "singlePlayer",
+                "XO",
+                new ArrayList<Player>(){{
+                    add(new Player("player", Figure.O));
+                    add(new Player("AI", Figure.X));
+                }},
+                new Field(3)
+        ) {{
+            setTurn(Figure.X);
+            setWinner(null);
         }};
 
-        String urlTemplate = "/game/" + UUID.randomUUID() + "/turn";
-        try {
-            this.mockMvc.perform(post(urlTemplate)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(turnBody)));
-            fail();
-        } catch (Exception e) {
+        dao.create(game);
+        return game;
+    }
 
+    private String readFromJson(String json)
+            throws IOException {
+        InputStream inputStream = this.getClass().getResourceAsStream(json);
+        StringBuilder resultStringBuilder = new StringBuilder();
+        try (BufferedReader br
+                     = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                resultStringBuilder.append(line).append("\n");
+            }
         }
+        return resultStringBuilder.toString();
     }
 
+    private void mockMvcGetRequest(String urlTemplate,
+                                   String response,
+                                   ResultMatcher resultMatcher) throws Exception {
 
-    private Game gameFromMvcResult(MvcResult result) throws Exception {
-        return objectMapper.readValue(result.getResponse().getContentAsString(), Game.class);
+        mockMvc.perform(get(urlTemplate))
+                .andExpect(resultMatcher)
+                .andExpect(content().json(response));
     }
 
-    private Game postAndReturnGame() throws Exception {
-        String requestBody1 = "{\"side\":\"X\"}";
-        MvcResult result = this.mockMvc.perform(post("/game")
+    private String createGameAndReturnId(Dao dao) {
+        Game expectedGame = createGameXO(dao);
+        return expectedGame.getId().toString();
+    }
+
+    private MvcResult mockMvcPostRequest(String urlTemplate,
+                                         String request,
+                                         String response,
+                                         ResultMatcher resultMatcher) throws Exception {
+
+        return  mockMvc.perform(post(urlTemplate)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(requestBody1))
-                .andExpect(status().isOk())
+                .content(request))
+                .andExpect(resultMatcher)
+                .andExpect(content().json(response))
                 .andReturn();
-
-        return gameFromMvcResult(result);
-    }
-
-    private  MvcResult postIncorrectBody(String requestBody) throws Exception {
-        MvcResult result = this.mockMvc.perform(post("/game")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(requestBody))
-                .andExpect(status().isBadRequest())
-                .andReturn();
-
-        return result;
     }
 
 }
