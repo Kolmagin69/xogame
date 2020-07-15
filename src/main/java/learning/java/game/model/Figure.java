39060de2c6beee0fb82ec2ade3b90654ac91c03a
Kolmagin69 <nikone.12.95@yandex.ru>
2020-07-15 23:14:41 +0300

    add static method figureFromString()

--- src/main/java/learning/java/game/model/Figure.java
+++ src/main/java/learning/java/game/model/Figure.java
@@ -1,6 +1,15 @@
 package learning.java.game.model;
 
 public enum Figure {
-    X, O
+    X, O;
 
+    public static Figure figureFromString(String string) {
+        if (string == null || "null".equals(string))
+            return null;
+        for (Figure figure: Figure.values()) {
+            if ((string.equals(figure.toString())))
+            return figure;
+        }
+        throw new IllegalArgumentException("Not found enum Figure." + string);
+    }
 }
