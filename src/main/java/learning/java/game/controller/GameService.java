930e937f42274718527be062993e621d735863c4
Kolmagin69 <nikone.12.95@yandex.ru>
2020-07-15 23:21:12 +0300

    add throws SQLException to method

--- src/main/java/learning/java/game/controller/GameService.java
+++ src/main/java/learning/java/game/controller/GameService.java
@@ -4,11 +4,13 @@ import learning.java.game.model.Game;
 import learning.java.game.rest.request.CreateGameRequest;
 import learning.java.game.rest.request.TurnGameRequest;
 
+import java.sql.SQLException;
+
 public interface GameService {
 
-    Game postNewGame(CreateGameRequest createGameRequest);
+    Game postNewGame(CreateGameRequest createGameRequest) throws SQLException;
 
-    Game getGameFromId(String id);
+    Game getGameFromId(String id) throws SQLException;
 
-    Game turnGameFromId(TurnGameRequest turnGameRequest, String id);
+    Game turnGameFromId(TurnGameRequest turnGameRequest, String id) throws SQLException;
 }
