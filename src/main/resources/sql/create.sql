3d18d86db03c3aba8b9ca2a83c45997b5ac5cb7a
Kolmagin69 <nikone.12.95@yandex.ru>
2020-08-03 09:53:36 +0300

    change the structure database resolve #1

--- src/main/resources/sql/create.sql
+++ src/main/resources/sql/create.sql
@@ -1,25 +1,36 @@
 CREATE USER xogame WITH PASSWORD '123321';
 CREATE DATABASE xogame;
-GRANT ALL PRIVILEGES  DATABASE xogame TO xogame;
+GRANT ALL PRIVILEGES ON DATABASE xogame TO xogame;
+
 
 CREATE TABLE fields(
 	id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-	size int,
+	size int NOT NULL,
 	figures varchar(1)[][]
-);
-
-CREATE TABLE games (
-	id uuid PRIMARY KEY,
-	type varchar(255),
-	name varchar(255),
-	turn varchar(1),
-	winner varchar(1),
-	field_id int REFERENCES fields(id)
 );
 
 CREATE TABLE players (
 	id uuid PRIMARY KEY,
 	name varchar(255),
-	figure varchar(1),
-	game_id uuid REFERENCES GAMES(id)
 );
+
+CREATE TABLE player_figures (
+    id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    player uuid REFERENCES players(id),
+    figure id REFERENCES figures(id)
+);
+
+CREATE TABLE games (
+	id uuid PRIMARY KEY,
+	type varchar(255) NOT NULL,
+	name varchar(255) NOT NULL,
+	turn varchar(1) NOT NULL,
+	winner varchar(1),
+	field_id int,
+	player_1 int,
+	player_2 int,
+	FOREIGN KEY (field_id) REFERENCES fields(id) ON DELETE CASCADE,
+	FOREIGN KEY (player_1) REFERENCES player_figures(id) ON DELETE CASCADE,
+	FOREIGN KEY (player_2) REFERENCES player_figures(id) ON DELETE CASCADE
+);
+
