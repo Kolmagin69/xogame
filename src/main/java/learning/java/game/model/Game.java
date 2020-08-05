28928698e2198e1154abedfb542f9cbf600cefcc
Kolmagin69 <nikone.12.95@yandex.ru>
2020-08-05 10:03:58 +0300

    changed due to changed structure DB

--- src/main/java/learning/java/game/model/Game.java
+++ src/main/java/learning/java/game/model/Game.java
@@ -1,6 +1,5 @@
 package learning.java.game.model;
 
-import java.util.List;
 import java.util.UUID;
 
 public class Game {
@@ -11,43 +10,64 @@ public class Game {
 
     private String name;
 
-    private Player player1;
+    private PlayerFigure player1;
 
-    private Player player2;
+    private PlayerFigure player2;
 
     private Field field;
 
-    private Figure turn = Figure.X;
+    private Figure turn;
 
-    private Figure winner = null;
+    private Figure winner;
 
-    public Game() {
-
-    }
-
-    public Game(String type, String name, Player player1, Player player2, Field field) {
-        this.field = field;
-        this.type = type;
-        this.name = name;
-        this.player1 = player1;
-        this.player1 = player2;
-    }
+    public Game() {}
 
     public UUID getId() {
         return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
     }
 
     public String getType() {
         return type;
     }
 
+    public void setType(String type) {
+        this.type = type;
+    }
+
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public PlayerFigure getPlayer1() {
+        return player1;
+    }
+
+    public void setPlayer1(PlayerFigure player1) {
+        this.player1 = player1;
+    }
+
+    public PlayerFigure getPlayer2() {
+        return player2;
+    }
+
+    public void setPlayer2(PlayerFigure player2) {
+        this.player2 = player2;
+    }
 
     public Field getField() {
         return field;
+    }
+
+    public void setField(Field field) {
+        this.field = field;
     }
 
     public Figure getTurn() {
@@ -65,38 +85,4 @@ public class Game {
     public void setWinner(Figure winner) {
         this.winner = winner;
     }
-
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Player getPlayer1() {
-        return player1;
-    }
-
-    public void setPlayer1(Player player1) {
-        this.player1 = player1;
-    }
-
-    public Player getPlayer2() {
-        return player2;
-    }
-
-    public void setPlayer2(Player player2) {
-        this.player2 = player2;
-    }
-
-    public void setField(Field field) {
-        this.field = field;
-    }
-
 }
--- src/main/java/learning/java/game/model/Player.java
+++ src/main/java/learning/java/game/model/Player.java
@@ -1,7 +1,5 @@
 package learning.java.game.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import java.util.UUID;
 
 public class Player {
@@ -10,25 +8,15 @@ public class Player {
 
     private String name;
 
-    private Figure figure;
-
-    @JsonIgnore
-    private UUID gameId;
-
     public Player() {
     }
 
-    public Player(String name, Figure figure) {
+    public Player(String name) {
         this.name = name;
-        this.figure = figure;
     }
 
     public String getName() {
         return name;
-    }
-
-    public Figure getFigure() {
-        return figure;
     }
 
     public UUID getId() {
@@ -43,15 +31,4 @@ public class Player {
         this.name = name;
     }
 
-    public void setFigure(Figure figure) {
-        this.figure = figure;
-    }
-
-    public UUID getGameId() {
-        return gameId;
-    }
-
-    public void setGameId(UUID gameId) {
-        this.gameId = gameId;
-    }
 }
