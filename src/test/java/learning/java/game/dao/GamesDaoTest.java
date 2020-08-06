27f6259c2eb0991972db08d0685d0446d7de3d35
Kolmagin69 <nikone.12.95@yandex.ru>
2020-08-06 23:54:00 +0300

    changed due to changes games model

--- src/test/java/learning/java/game/dao/GamesDaoTest.java
+++ src/test/java/learning/java/game/dao/GamesDaoTest.java
@@ -1,15 +1,11 @@
 package learning.java.game.dao;
 
 import learning.java.game.controller.GameControllerSingle;
-import learning.java.game.model.Figure;
-import learning.java.game.model.Game;
-import learning.java.game.model.Point;
+import learning.java.game.model.*;
 import org.junit.jupiter.api.Test;
 
-import java.sql.SQLException;
-import java.util.*;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.LinkedList;
+import java.util.UUID;
 
 class GamesDaoTest {
 
@@ -22,22 +18,22 @@ class GamesDaoTest {
     private final LinkedList<UUID> gameKeySet = new LinkedList<>();
 
     @Test
-    void create() throws SQLException {
-        Game game = gameController.newGame(Figure.X);
-        playersDao.create(game.getPlayer1().getPlayer());
-        playersDao.create(game.getPlayer2().getPlayer());
+    void create() {
+        Player player = new Player("Max");
+        playersDao.create(player);
+        Game game = gameController.newGame(Figure.X, player);
         gameKeySet.add(dao.create(game));
     }
 
     @Test
-    void read() throws SQLException {
+    void read() {
         create();
         Game game =  dao.read(gameKeySet.getLast());
 
     }
 
     @Test
-    void update() throws  SQLException {
+    void update() {
         create();
         Game game = dao.read(gameKeySet.getLast());
         game.setWinner(Figure.O);
@@ -47,7 +43,7 @@ class GamesDaoTest {
     }
 
     @Test
-    void delete() throws SQLException {
+    void delete() {
         create();
         UUID id = gameKeySet.getLast();
         Game game = dao.read(id);
