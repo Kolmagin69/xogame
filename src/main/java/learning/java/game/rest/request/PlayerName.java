90ae6d3d83b912fb30509f3980955779f4f568f8
Kolmagin69 <nikone.12.95@yandex.ru>
2020-08-07 00:04:59 +0300

    add PlayerName.java

---
+++ src/main/java/learning/java/game/rest/request/PlayerName.java
@@ -0,0 +1,18 @@
+package learning.java.game.rest.request;
+
+public class PlayerName {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+
+}
