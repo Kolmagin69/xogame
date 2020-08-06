d57b28b5d4badcbf8e697c3dd525dc4da1ba315a
Kolmagin69 <nikone.12.95@yandex.ru>
2020-08-07 00:11:50 +0300

    remove throws SQLException in signature methods

--- src/main/java/learning/java/game/controller/GameService.java
+++ src/main/java/learning/java/game/controller/GameService.java
@@ -4,13 +4,11 @@ import learning.java.game.model.Game;
 import learning.java.game.rest.request.CreateGameRequest;
 import learning.java.game.rest.request.TurnGameRequest;
 
-import java.sql.SQLException;
-
 public interface GameService {
 
-    Game postNewGame(CreateGameRequest createGameRequest) throws SQLException;
+    Game postNewGame(CreateGameRequest createGameRequest);
 
-    Game getGameFromId(String id) throws SQLException;
+    Game getGameFromId(String id);
 
-    Game turnGameFromId(TurnGameRequest turnGameRequest, String id) throws SQLException;
+    Game turnGameFromId(TurnGameRequest turnGameRequest, String id);
 }
