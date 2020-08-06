9ecc7c2bc44d7a002e72a5969dfa2ac4c542d67c
Kolmagin69 <nikone.12.95@yandex.ru>
2020-08-07 00:01:26 +0300

    remove throws SQLException into methods

--- src/main/java/learning/java/game/dao/Dao.java
+++ src/main/java/learning/java/game/dao/Dao.java
@@ -1,15 +1,13 @@
 package learning.java.game.dao;
 
-import java.sql.SQLException;
-
 public interface Dao<Entity, Key> {
 
-    Key create(Entity entity) throws SQLException;
+    Key create(Entity entity) ;
 
-    Entity read(Key key) throws SQLException;
+    Entity read(Key key) ;
 
-    Key update(Entity entity) throws SQLException;
+    Key update(Entity entity) ;
 
-    boolean delete(Entity entity) throws SQLException;
+    boolean delete(Entity entity) ;
 
 }
