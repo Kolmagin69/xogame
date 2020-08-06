a968a8be1f9da26bd5de926020ed6848d2d9dc74
Kolmagin69 <nikone.12.95@yandex.ru>
2020-08-07 00:05:38 +0300

    add field Player

--- src/main/java/learning/java/game/rest/request/CreateGameRequest.java
+++ src/main/java/learning/java/game/rest/request/CreateGameRequest.java
@@ -1,10 +1,15 @@
 package learning.java.game.rest.request;
 
 import learning.java.game.model.Figure;
+import learning.java.game.model.Player;
+
+import java.util.UUID;
 
 public class CreateGameRequest {
 
     private Figure side;
+
+    private Player player;
 
     public Figure getSide() {
         return side;
@@ -12,5 +17,13 @@ public class CreateGameRequest {
 
     public void setSide(Figure side) {
         this.side = side;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
     }
 }
