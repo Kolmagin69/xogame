8a477f0dd2f45ff70bd61ddadcd87c7699e2a263
Kolmagin69 <nikone.12.95@yandex.ru>
2020-08-06 23:55:17 +0300

    add FunctionalInterface<T1,T2,R>

---
+++ src/main/java/learning/java/game/dao/SQLFunctionTwoType.java
@@ -0,0 +1,9 @@
+package learning.java.game.dao;
+
+import java.sql.SQLException;
+
+@FunctionalInterface
+public interface SQLFunctionTwoType<T1,T2,R> {
+
+     R apply(T1 t1, T2 t2) throws SQLException;
+}
