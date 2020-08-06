89bc939bc66ea7687f931683e03518989b6d7538
Kolmagin69 <nikone.12.95@yandex.ru>
2020-08-07 00:07:22 +0300

    changed returning value Game to GameTO

--- src/main/java/learning/java/game/rest/GameRestController.java
+++ src/main/java/learning/java/game/rest/GameRestController.java
@@ -1,13 +1,12 @@
 package learning.java.game.rest;
 
 import learning.java.game.controller.GameService;
-import learning.java.game.model.Game;
+import learning.java.game.rest.item.GameMapping;
+import learning.java.game.rest.item.GameTO;
 import learning.java.game.rest.request.CreateGameRequest;
 import learning.java.game.rest.request.TurnGameRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.sql.SQLException;
 
 @RestController
 @RequestMapping("game")
@@ -16,24 +15,25 @@ public class GameRestController {
     @Autowired
     GameService gameService;
 
+    @Autowired
+    GameMapping mapping;
+
     @GetMapping("{id}")
     @ResponseBody
-    public Game gameFromId(@PathVariable String id) throws SQLException {
-        return gameService.getGameFromId(id);
+    public GameTO gameFromId(@PathVariable String id) {
+        return mapping.gameTOFromGame(gameService.getGameFromId(id));
     }
 
     @PostMapping
-    public @ResponseBody Game postGame(@RequestBody CreateGameRequest createGameRequest) throws SQLException {
-        return gameService.postNewGame(createGameRequest);
+    public @ResponseBody GameTO postGame(@RequestBody CreateGameRequest createGameRequest) {
+        return mapping.gameTOFromGame(gameService.postNewGame(createGameRequest));
     }
 
     @PostMapping("{id}/turn")
-    public  @ResponseBody Game turn(@RequestBody TurnGameRequest turnGameRequest,
-                                     @PathVariable String id) throws SQLException {
+    public  @ResponseBody GameTO turn(@RequestBody TurnGameRequest turnGameRequest,
+                                     @PathVariable String id) {
 
-        return gameService.turnGameFromId(turnGameRequest, id);
+        return mapping.gameTOFromGame(gameService.turnGameFromId(turnGameRequest, id));
     }
-
-
 }
 
