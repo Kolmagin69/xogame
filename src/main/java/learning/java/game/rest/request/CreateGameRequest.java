8747d042238379840357e7b3551e9c851afa1989
Kolmagin69 <nikone.12.95@yandex.ru>
2020-08-17 11:50:11 +0300

    changed request body

--- src/main/java/learning/java/game/rest/request/CreateGameRequest.java
+++ src/main/java/learning/java/game/rest/request/CreateGameRequest.java
@@ -3,35 +3,37 @@ package learning.java.game.rest.request;
 import learning.java.game.model.Figure;
 import learning.java.game.model.Player;
 
+import java.util.UUID;
+
 public class CreateGameRequest {
 
-    private Figure side;
+    private Figure sidePlayer1;
 
-    private Player player1;
+    private UUID playerId1;
 
-    private Player player2;
+    private UUID playerId2;
 
-    public Figure getSide() {
-        return side;
+    public Figure getSidePlayer1() {
+        return sidePlayer1;
     }
 
-    public void setSide(Figure side) {
-        this.side = side;
+    public void setSidePlayer1(Figure sidePlayer1) {
+        this.sidePlayer1 = sidePlayer1;
     }
 
-    public Player getPlayer1() {
-        return player1;
+    public UUID getPlayerId1() {
+        return playerId1;
     }
 
-    public void setPlayer1(Player player1) {
-        this.player1 = player1;
+    public void setPlayerId1(UUID playerId1) {
+        this.playerId1 = playerId1;
     }
 
-    public Player getPlayer2() {
-        return player2;
+    public UUID getPlayerId2() {
+        return playerId2;
     }
 
-    public void setPlayer2(Player player2) {
-        this.player2 = player2;
+    public void setPlayerId2(UUID playerId2) {
+        this.playerId2 = playerId2;
     }
 }
