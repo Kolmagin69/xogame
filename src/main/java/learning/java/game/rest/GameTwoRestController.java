15c4f539c2f5686de24cf4b7f95cedb54378feff
Kolmagin69 <nikone.12.95@yandex.ru>
2020-08-17 11:53:02 +0300

    add GameTwoRestController for mapping game with two player

---
+++ src/main/java/learning/java/game/rest/GameTwoRestController.java
@@ -0,0 +1,41 @@
+package learning.java.game.rest;
+
+import learning.java.game.controller.GameService;
+import learning.java.game.rest.item.GameMapping;
+import learning.java.game.rest.item.GameTO;
+import learning.java.game.rest.request.CreateGameRequest;
+import learning.java.game.rest.request.TurnGameRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("game/2")
+public class GameTwoRestController {
+
+    @Autowired
+    @Qualifier("gameServiceTwoBean")
+    GameService gameService;
+
+    @Autowired
+    GameMapping mapping;
+
+    @GetMapping("{id}")
+    @ResponseBody
+    public GameTO gameFromId(@PathVariable String id) {
+        return mapping.gameTOFromGame(gameService.getGameFromId(id));
+    }
+
+    @PostMapping
+    public @ResponseBody GameTO postGame(@RequestBody CreateGameRequest createGameRequest) {
+        return mapping.gameTOFromGame(gameService.postNewGame(createGameRequest));
+    }
+
+    @PostMapping("{id}/turn")
+    public  @ResponseBody GameTO turn(@RequestBody TurnGameRequest turnGameRequest,
+                                     @PathVariable String id) {
+
+        return mapping.gameTOFromGame(gameService.turnGameFromId(turnGameRequest, id));
+    }
+}
+
