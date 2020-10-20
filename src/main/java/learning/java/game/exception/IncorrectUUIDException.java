8e1e8fbc9f7b3c1528670c799081a3888302b3b6
Kolmagin69 <nikone.12.95@yandex.ru>
2020-10-20 17:58:47 +0300

    created IncorrectUUIDException

---
+++ src/main/java/learning/java/game/exception/IncorrectUUIDException.java
@@ -0,0 +1,7 @@
+package learning.java.game.exception;
+
+public class IncorrectUUIDException extends RuntimeException{
+    public  IncorrectUUIDException(String message){
+        super(message);
+    }
+}
